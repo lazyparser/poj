5ddf539fa8246eea2fb9673cfd7b943661261b12
lazyparser <lazyparser@gmail.com>
2016-01-27 09:43:25 +0800

    COPY 1001 from http://poj.org/bbs

---
+++ 1001/1001.java
@@ -0,0 +1,38 @@
+// This code is copied from
+//     http://poj.org/showmessage?message_id=164084
+// Rename file to 'Main' if you want to compile it on your machine.
+
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+	static Scanner cin = new Scanner(System.in);
+	static PrintWriter cout = new PrintWriter(System.out, true);
+
+	public static void main(String[] args)
+	{
+		BigDecimal a,c;
+		int b;
+		while (cin.hasNext())
+		{
+			a = cin.nextBigDecimal();
+			b = cin.nextInt();
+			c = a.pow(b);
+			String ans = c.toPlainString();
+			if (ans.contains(".") == false)
+			{
+				cout.println(ans);
+			}
+			else
+			{
+				int x = 0, y = ans.length() - 1;
+				while (ans.charAt(x) == '0') x++;
+				while (ans.charAt(y) == '0') y--;
+				if (ans.charAt(y) != '.') y++;
+				cout.println(ans.substring(x, y));
+			}
+
+		}
+	}
+}
